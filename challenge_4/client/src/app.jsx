3ebd878ff395f5e83164fee218025cea7f7b5dba
Seanlangbrown <seanlangbrown@gmail.com>
2018-04-07 12:01:18 -0700

    debug react components

---
+++ challenge_4/client/src/app.jsx
@@ -0,0 +1,19 @@
+import react from 'react';
+import _ from 'lodash';
+
+class App extends React.Component {
+
+  constructor () {
+    super();
+  }
+
+
+  render () {
+    return (
+      'hello'
+    );
+  }
+}
+
+export default App;
+
